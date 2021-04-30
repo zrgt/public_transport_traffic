dad467637494d1940cfc32535b80a0441e98c66b
zrgt <garmaev@gmx.de>
2021-04-30 19:08:00 +0200

    Fix bug with speed func (change lat and lon)

--- utils.py
+++ utils.py
@@ -1,11 +1,15 @@
 import math
 
 
-def pos_to_meters(lat1, lon1, lat2, lon2): # generally used geo measurement function
-    lat1 = lat1/10**6
-    lon1 = lon1/10**6
-    lat2 = lat2/10**6
-    lon2 = lon2/10**6
+def distance(lat1, lon1, lat2, lon2): # generally used geo measurement function
+    if lat1 > 10**6:
+        lat1 = lat1/10**6
+    if lon1 > 10 ** 6:
+        lon1 = lon1/10**6
+    if lat2 > 10 ** 6:
+        lat2 = lat2/10**6
+    if lon2 > 10 ** 6:
+        lon2 = lon2/10**6
 
     R = 6378.137 # Radius of earth in KM
     dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
@@ -16,14 +20,18 @@ def pos_to_meters(lat1, lon1, lat2, lon2): # generally used geo measurement func
     d = R * c
     return d * 1000 # meters
 
+# pos_to_meters(51829772, 107584201, 49982046, 107564452) vertical
+# pos_to_meters(51829772, 107584201, 52284568, 104283160)
 
-def speed(lon1, lat1, lon2, lat2, time1, time2):
-    distance = pos_to_meters(lon1, lat1, lon2, lat2)
+# 52.284568, 104.283160
+
+def speed(lat1, lon1, lat2, lon2, time1, time2):
+    distance = distance(lat1, lon1, lat2, lon2)
     tdelta = time2 - time1
 
-    m_per_sec = distance/tdelta.total_seconds()
-    kmh = m_per_sec*60*60/1000
-    return kmh
+    speed_mps = distance/tdelta.total_seconds()
+    speed_kmh = (speed_mps*60*60)/1000
+    return speed_kmh
 
 
 def geographic_to_web_mercator(x_lon, y_lat):
@@ -43,9 +51,9 @@ def lat_to_y_mercator(lat):
 
 
 def speed_color(kmh):
-    if kmh < 7:
+    if kmh < 8:
         return "red"
-    elif kmh < 15:
+    elif kmh < 18:
         return "yellow"
     else:
         return "green"
