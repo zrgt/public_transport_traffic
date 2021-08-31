fe767611fe9625294b88f45a5674e02ccd14bde5
zrgt <garmaev@gmx.de>
2021-08-31 04:02:27 +0200

    Refactor script

--- bokeh_visual_all_pd.py
+++ bokeh_visual_all_pd.py
@@ -1,61 +1,24 @@
-# importing the modules
 import datetime
 import json
 
 from bokeh.layouts import column, row
 from bokeh.models import Slider, CustomJS, ColumnDataSource, Button, WheelZoomTool, CheckboxGroup, \
-    CustomJSFilter, CDSView, Spinner
+    CustomJSFilter, CDSView, Spinner, RangeSlider
 from bokeh.plotting import show
-import pandas as pd
-import numpy as np
 from scipy.spatial.distance import pdist, squareform
 import bokeh.models as bmo
 
-from graph_utils import init_map_graph
-from utils import lon_to_x_mercator, lat_to_y_mercator, pd_speed_color
+from dataframe import get_dataframe
+from graph_utils import init_map_graph, get_checkboxes_with_filter
 
 start_time = datetime.datetime.now()
 FILE = f"all_pos_8_9.json"
-ROUTES_LIST = ["Тр-1", "Тр-2", "Тр-4"]
 TIMESTAMPS = 180
 
 #init graph
 graph = init_map_graph(title="Средняя скорость трамвая в понедельник 19 апреля 2021 между 8-9ч")
 
-with open(FILE, "r") as file:
-    geo_marker_dicts_lists = list(json.load(file).values())
-geo_markers = []
-for lst in geo_marker_dicts_lists:
-    geo_markers.extend(lst)
-df = pd.DataFrame(geo_markers)
-df.drop_duplicates(inplace=True)
-df.drop(["low_floor"], axis=1, inplace=True)
-indexNotTrams = df[df["rtype"] != "Тр"].index
-df.drop(index=indexNotTrams, inplace=True)
-df["lon"] = lon_to_x_mercator(df["lon"])
-df["lat"] = lat_to_y_mercator(df["lat"])
-df["route"] = df["rtype"]+"-"+df["rnum"]
-def fix_time(row):
-    time = datetime.datetime.strptime(row.lasttime, "%d.%m.%Y %H:%M:%S")
-    time = time.replace(hour=time.hour + 8)
-    row.lasttime = time.strftime("%H:%M:%S")
-    return row
-df = df.apply(fix_time, axis="columns")
-df["color"] = ""
-df = df.apply(pd_speed_color, axis="columns")
-
-pos = df[(df.speed < 8)][["lat", "lon"]]
-sf = squareform(pdist(pos))
-pos["num_near_points"] = (sf < 100).sum(axis=1) / 2.5
-df["size"] = pos["num_near_points"]
-df["size"].fillna(7, inplace=True)
-df["size"] = df["size"].astype("int32")
-
-df.rename(columns={"lon":"x", "lat":"y", "lasttime":"time", "rnum":"num"}, inplace=True)
-print(df.head)
-
-end_time = datetime.datetime.now()
-print(end_time - start_time)
+df = get_dataframe(FILE)
 
 source_visible = ColumnDataSource(df)
 
@@ -86,28 +49,16 @@ return indices;
 speed_checkboxes.js_on_change("active", CustomJS(code="source.change.emit();", args=dict(source=source_visible)))
 
 # routes checkboxes
-routes_checkboxes = CheckboxGroup(labels=ROUTES_LIST, active=list(range(len(ROUTES_LIST))))
+routes_list = list(df.route.unique())
+routes_list.sort()
+routes_checkboxes, routes_filter = get_checkboxes_with_filter(labels=routes_list, column_label="route", source=source_visible,
+                                                              select_all_btn=False, clear_all_btn=False)
+# gos num checkboxes
+gos_num_list = list(df.gos_num.unique())
+gos_num_list.sort()
+gos_num_checkboxes, gos_num_filter = get_checkboxes_with_filter(labels=gos_num_list, column_label="gos_num", source=source_visible)
 
-routes_filter = CustomJSFilter(code='''
-var selected = checkboxes.active.map(i=>checkboxes.labels[i]);
-var indices = [];
-var column = source.data['route'];
-
-// iterate through rows of data source and see if each satisfies some constraint
-for (var i = 0; i < column.length; i++){
-    if(selected.includes(column[i])){
-        indices.push(true);
-    } else {
-        indices.push(false);
-    }
-}
-console.log("filter completed");
-return indices;
-'''
-                               , args={'checkboxes': routes_checkboxes})
-routes_checkboxes.js_on_change("active", CustomJS(code="source.change.emit();", args=dict(source=source_visible)))
-
-view = CDSView(source=source_visible, filters=[speed_filter, routes_filter])
+view = CDSView(source=source_visible, filters=[speed_filter, routes_filter, gos_num_filter])
 # plotting the graph
 points = graph.scatter("x", "y", source=source_visible, fill_color="color", size="size",
                        fill_alpha=0.3, line_color=None, view=view)
@@ -116,4 +67,5 @@ spinner = Spinner(title="Points size", low=1, high=40, step=1, value=7, width=80
 
 layout = row(graph, column(spinner, speed_checkboxes, routes_checkboxes))
 # displaying the model
+layout = row(column(graph, range_slider), column(spinner, speed_checkboxes), routes_checkboxes, gos_num_checkboxes)
 show(layout)
