b3d72fc308f9c3c4004da6c07d548bc3c7fc4890
zrgt <garmaev@gmx.de>
2021-08-31 04:01:32 +0200

    Add get_dataframe()

---
+++ dataframe.py
@@ -0,0 +1,67 @@
+import datetime
+import json
+
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform
+
+from utils import lon_to_x_mercator, lat_to_y_mercator, pd_speed_color
+
+start_time = datetime.datetime.now()
+FILE = f"all_pos_8_9.json"
+
+
+def get_dataframe(file):
+    # Open data file
+    with open(file, "r", encoding='utf-8') as file:
+        # List with lists for each parse iteration
+        geo_marker_dicts_lists = list(json.load(file).values())
+
+    # Export all lists in one big list
+    geo_markers = []
+    for lst in geo_marker_dicts_lists:
+        geo_markers.extend(lst)
+
+    df = pd.DataFrame(geo_markers)
+    df.drop_duplicates(inplace=True)
+    # drop useless data column
+    df.drop(["low_floor"], axis=1, inplace=True)
+    # drop all vehicles except trams
+    indexNotTrams = df[df["rtype"] != "Тр"].index
+    df.drop(index=indexNotTrams, inplace=True)
+    # change geo data in standard for maps
+    df["lon"] = lon_to_x_mercator(df["lon"])
+    df["lat"] = lat_to_y_mercator(df["lat"])
+
+    df["route"] = df["rtype"]+"-"+df["rnum"]
+
+    # fix time: drop day data and change timezone
+    def fix_time(row):
+        time = datetime.datetime.strptime(row.lasttime, "%d.%m.%Y %H:%M:%S")
+        time = time.replace(hour=time.hour + 8)
+        row.lasttime = time.strftime("%H:%M:%S")
+        return row
+    df = df.apply(fix_time, axis="columns")
+
+    # add column data for point color and fill it with data
+    df["color"] = ""
+    df = df.apply(pd_speed_color, axis="columns")
+
+    # make red (slow) points bigger if there are slow points near
+    pos = df[(df.speed < 8)][["lat", "lon"]]
+    sf = squareform(pdist(pos))
+    pos["num_near_points"] = (sf < 100).sum(axis=1) / 2.5
+    df["size"] = pos["num_near_points"]
+    df["size"].fillna(7, inplace=True)
+    df["size"] = df["size"].astype("int32")
+
+    # rename data columns in dataframe
+    df.rename(columns={"lon":"x", "lat":"y", "lasttime":"time", "rnum":"num"}, inplace=True)
+    print(df.head)
+
+    end_time = datetime.datetime.now()
+    print(end_time - start_time)
+
+    return df
+
+if __name__ == "__main__":
+    get_dataframe(FILE)
